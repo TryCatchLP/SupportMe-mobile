fcaecbbad200d380fdb392a3d144ce07e1f7a7ac
Yuli <ykgarcia@espol.edu.ec>
2020-09-16 01:39:35 -0500

    buscar-filtro

--- lib/views/buscar.dart
+++ lib/views/buscar.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+﻿// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -7,14 +7,23 @@ import 'package:supportme/models/hueca.dart';
 import 'package:supportme/services/hueca_service.dart';
 
 class BuscarView extends StatelessWidget {
+  final Function(Hueca hueca) onClose;
+
+  const BuscarView({Key key, this.onClose}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Buscar();
+    return Buscar(
+      onClose: onClose
+    );
   }
 }
 
 class Buscar extends StatefulWidget {
   final List<String> list = null;
+  final Function(Hueca hueca) onClose;
+
+  const Buscar({Key key, this.onClose}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -39,8 +48,10 @@ class _HomePageState extends State<Buscar> {
         //backgroundColor: PrimaryColor,
         actions: <Widget>[
           IconButton(
-            onPressed: () {
-              showSearch(context: context, delegate: Search(widget.list));
+            onPressed: () async {
+              Hueca hueca =
+                  await showSearch(context: context, delegate: Search());
+              this.widget.onClose(hueca);
             },
             icon: Icon(Icons.search),
           ),
@@ -99,13 +110,22 @@ class _HomePageState extends State<Buscar> {
         child: Row(children: <Widget>[
           SizedBox(height: 25.0),
           SizedBox(width: 12.0),
-          Image.network(
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSnekR0L0HHNjpeva06BBuq1oH44lplGESXNQ&usqp=CAU",
-              width: 70),
+          FadeInImage(
+            placeholder: AssetImage("assets/images/bg.jpg"),
+            image: NetworkImage(
+                huecas[i]?.photo != null && huecas[i]?.photo != "C://"
+                    ? "http://localhost:8000/${huecas[i]?.photo}"
+                    : "https://pbs.twimg.com/media/D38wwCmW4AEp8lo.jpg"),
+            width: 70,
+          ),
           SizedBox(width: 10.0),
-          Text(
-              "${huecas[i].name}\n${huecas[i].descrip}\nAbierto\nLunes-Viernes: 07:30-16:00\nS�bado: 8:00-15:00",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11)),
+          Container(
+            constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.5),
+            child: Text(
+                "${huecas[i].name}\n${huecas[i].descrip}\nAbierto\n${huecas[i].schedule}",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11)),
+          ),
         ]));
   }
 
@@ -139,7 +159,7 @@ class _HomePageState extends State<Buscar> {
   }
 }
 
-class Search extends SearchDelegate {
+class Search extends SearchDelegate<Hueca> {
   @override
   List<Widget> buildActions(BuildContext context) {
     return <Widget>[
@@ -166,20 +186,72 @@ class Search extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(selectedResult),
-      ),
+    if (query.trim().length == 0) {
+      return Text("Sin resultados");
+    }
+
+    return FutureBuilder(
+        future: HuecaService.getHuecas(query: this.query),
+        builder: (context, AsyncSnapshot<List<Hueca>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return _showResults(snapshot.data);
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+
+  Widget _showResults(List<Hueca> huecas) {
+    return ListView.builder(
+        itemBuilder: (c, i) => _builder(c, huecas[i]),
+        itemCount: huecas.length);
+  }
+
+  Widget _builder(BuildContext context, Hueca hueca) {
+    return GestureDetector(
+      onTap: () {
+        close(context, hueca);
+      },
+      child: Container(
+          margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20),
+          decoration: BoxDecoration(
+            color: Color(0xFFF1D57F),
+            border: Border.all(
+              color: Color(0xFFF1D57F),
+              width: 1.0,
+            ),
+          ),
+          child: Row(children: <Widget>[
+            SizedBox(height: 25.0),
+            SizedBox(width: 12.0),
+            FadeInImage(
+              placeholder: AssetImage("assets/images/bg.jpg"),
+              image: NetworkImage(hueca?.photo != null && hueca?.photo != "C://"
+                  ? "http://localhost:8000/${hueca?.photo}"
+                  : "https://pbs.twimg.com/media/D38wwCmW4AEp8lo.jpg"),
+              width: 70,
+            ),
+            SizedBox(width: 10.0),
+            Container(
+              constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.5),
+              child: Text(
+                  "${hueca.name}\n${hueca.descrip}\nAbierto\n${hueca.schedule}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11)),
+            ),
+          ])),
     );
   }
 
-  final List<String> listExample;
-  Search(this.listExample);
+  Search();
 
   List<String> recentList = ["Menu", "Menu 1"];
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Column();
+    return ListTile(
+      title: Text("Suggestions"),
+    );
   }
 }
